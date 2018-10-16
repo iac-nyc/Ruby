0f71d9cf9161077bbbde56f459653344245aa741
iac-nyc <007ifte@gmail.com>
2018-10-16 09:58:31 -0400

    hello_world

---
+++ basics/hello.rb
@@ -0,0 +1,17 @@
+puts "Hello, World!"
+
+puts("The answer to life, the universe and everything: ")
+puts(38)
+#puts("Answer 3 + 4:" + "4".to_i + "3".to_i)
+seven = "4".to_i + "3".to_i
+puts(seven)
+not_seven = "the answer: 4" + "3"
+puts(not_seven)
+puts(7 - 5)
+puts(3 * 4)
+puts(8 / 2)
+puts(9.0 / 2)
+puts(9 / 2.0)
+puts(9.fdiv(2))
+puts(11 / 4 )
+puts(11 % 4 )
